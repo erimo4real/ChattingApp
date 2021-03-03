8eb26d5c0c303e250a2dfebcd6eb199b0336cb06
erimo <eromoxlx@gmail.com>
2021-03-03 14:39:23 +0100

    step two

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,12 +35,14 @@ namespace API
              {
                  options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
              });
-             
+           
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,9 +55,12 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+             
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
